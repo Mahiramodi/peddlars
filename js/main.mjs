0f716c9be88f7f974b106b11aedeb29757fda475
Mahiramodi <68337662+Mahiramodi@users.noreply.github.com>
2021-11-13 00:54:59 +0530

    Qr scanner done

--- js/main.mjs
+++ js/main.mjs
@@ -14,20 +14,22 @@ const db = getFirestore();
 if (login_form) {
     login_form.addEventListener("submit", (e) => {
         e.preventDefault();
-        console.log("sub,itted");
+        console.log("submitted");
         const loginemail = login_form["email"].value;
         const loginpassword = login_form["password"].value;
         signInWithEmailAndPassword(auth, loginemail, loginpassword)
             .then((userCredential) => {
                 // Signed in 
                 console.log(userCredential.user);
-                // ...
+                alert("Signed In Successfully")
+                    // ...
             })
             .catch((error) => {
                 const errorCode = error.code;
                 const errorMessage = error.message;
                 console.log(errorCode);
             });
+
     });
 }
 
@@ -38,16 +40,28 @@ if (register_form) {
             register_form["password2"].value != "" && (register_form["password2"].value.length) > 6) {
 
             console.log("submitted")
-            var email = register_form["email2"].value;
-            var password = register_form["password2"].value
-            createUserWithEmailAndPassword(auth, email, password).then((cred) => {
-                console.log(cred);
-                setDoc(doc(db, "Users", email), {
+            var email2 = register_form["email2"].value;
+            var password2 = register_form["password2"].value
+            createUserWithEmailAndPassword(auth, email2, password2).then((cred) => {
+
+                setDoc(doc(db, "Users", email2), {
                     name: register_form["name2"].value,
-                    email: email,
+                    email: email2,
                     status: "clear",
-                });
+                }).then(() => {
+                    console.log(cred);
+                    alert("Account Created Successfully")
+                    window.location.replace("http://127.0.0.1:5500/peddlars/login2.html")
+                })
+
+
+
+
+
             })
+
+            // alert("Account Created Successfully")
+            // window.location.replace("http://127.0.0.1:5500/peddlars/login.html")
 
 
         } else {
